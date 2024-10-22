3e5723f312d2f2f961ee4a5c7bca4d59796d6514
MajorityEliminator <Kreefore@gmail.com>
2024-10-22 13:34:51 +0300

    Update №1 Замена пропущенного элемента средним арифметическим.py

--- Лабораторная работа №1/№1 Замена пропущенного элемента средним арифметическим.py
+++ Лабораторная работа №1/№1 Замена пропущенного элемента средним арифметическим.py
@@ -1,9 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 Noneid = numbers.index(None)
-numbers_without_none = numbers[0:Noneid] + numbers[Noneid+1:]
-sum_ = sum(numbers_without_none)
-avg = sum_/len(numbers)
+
+avg = (sum(numbers[0:Noneid]) + sum(numbers[Noneid+1:]))/len(numbers)
 numbers[Noneid] = avg
 
 print(f"Измененный список:", numbers)
